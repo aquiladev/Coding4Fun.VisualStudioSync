e87ddf8c83d175f00079e2eb7cd6d00c823d6d23
Sergii Bomko <aquila.ua@gmail.com>
2013-09-02 16:49:35 +0300

    added solution and some files

---
+++ VisualStudioSync.Live/LiveAuthForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VisualStudioSync.Live
+{
+	public delegate void AuthCompletedCallback(AuthResult result);
+
+	public partial class LiveAuthForm : Form
+	{
+		private readonly string _startUrl;
+		private readonly string _endUrl;
+		private readonly AuthCompletedCallback _callback;
+
+		public LiveAuthForm(string startUrl, string endUrl, AuthCompletedCallback callback)
+		{
+			_startUrl = startUrl;
+			_endUrl = endUrl;
+			_callback = callback;
+			InitializeComponent();
+		}
+
+		private void LiveAuthForm_Load(object sender, EventArgs e)
+		{
+			webBrowser.Navigated += WebBrowser_Navigated;
+			webBrowser.Navigate(_startUrl);
+		}
+
+		private void WebBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+		{
+			if (webBrowser.Url.AbsoluteUri.StartsWith(_endUrl))
+			{
+				if (_callback != null)
+				{
+					_callback(new AuthResult(this.webBrowser.Url));
+				}
+			}
+		}
+	}
+}
---
+++ VisualStudioSync.Live/LiveController.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Live;
+
+namespace VisualStudioSync.Live
+{
+	public class LiveController : IRefreshTokenHandler
+	{
+		private const string ClientId = "00000000481024B2";
+		private LiveAuthForm _authForm;
+		private LiveAuthClient _liveAuthClient;
+		private LiveConnectClient _liveConnectClient;
+		private RefreshTokenInfo _refreshTokenInfo;
+
+		private LiveAuthClient AuthClient
+		{
+			get
+			{
+				if (_liveAuthClient == null)
+				{
+					AuthClient = new LiveAuthClient(ClientId, this);
+				}
+				return _liveAuthClient;
+			}
+			set
+			{
+				if (_liveAuthClient != null)
+				{
+					_liveAuthClient.PropertyChanged -= liveAuthClient_PropertyChanged;
+				}
+				_liveAuthClient = value;
+				if (_liveAuthClient != null)
+				{
+					_liveAuthClient.PropertyChanged += liveAuthClient_PropertyChanged;
+				}
+				_liveConnectClient = null;
+			}
+		}
+
+		public LiveController()
+		{
+			InitLive();
+		}
+
+		private void liveAuthClient_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == "Session")
+			{
+			}
+		}
+
+		private async void InitLive()
+		{
+			var loginResult = await AuthClient.IntializeAsync();
+			if (loginResult.Session != null)
+			{
+				_liveConnectClient = new LiveConnectClient(loginResult.Session);
+			}
+		}
+
+		private void AuthForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			CleanupAuthForm();
+		}
+
+		private void CleanupAuthForm()
+		{
+			if (_authForm != null)
+			{
+				_authForm.Dispose();
+				_authForm = null;
+			}
+		}
+
+		public Task SaveRefreshTokenAsync(RefreshTokenInfo tokenInfo)
+		{
+			return Task.Factory.StartNew(() =>
+			{
+				_refreshTokenInfo = tokenInfo;
+			});
+		}
+
+		public Task<RefreshTokenInfo> RetrieveRefreshTokenAsync()
+		{
+			return Task.Factory.StartNew(() => _refreshTokenInfo);
+		}
+
+		public string GetFile()
+		{
+			DoCommand(DoIt);
+			return "qw";
+		}
+
+		private void DoCommand(AuthCompletedCallback action)
+		{
+			if (_authForm == null && _liveConnectClient == null)
+			{
+				var startUrl = AuthClient.GetLoginUrl(new List<string> { "wl.signin", "wl.skydrive", "wl.skydrive_update" });
+				const string endUrl = "https://login.live.com/oauth20_desktop.srf";
+				_authForm = new LiveAuthForm(
+					startUrl,
+					endUrl,
+					action);
+				_authForm.FormClosed += AuthForm_FormClosed;
+				_authForm.ShowDialog();
+			}
+		}
+
+		private async void DoIt(AuthResult result)
+		{
+			CleanupAuthForm();
+			if (result.AuthorizeCode != null)
+			{
+				var session = await AuthClient.ExchangeAuthCodeAsync(result.AuthorizeCode);
+				_liveConnectClient = new LiveConnectClient(session);
+
+				var operationResult = await _liveConnectClient.GetAsync("me/skydrive/files");
+				dynamic re = operationResult.Result;
+
+				//var items = re.Result["data"] as List<object>;
+				//foreach (object item in items)
+				//{
+				//	var file = item as IDictionary<string, object>;
+				//	if (file["name"].ToString() == "somefile.txt")
+				//	{
+				//		var id = file["id"].ToString();
+				//	}
+				//}
+			}
+		}
+	}
+}
